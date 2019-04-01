6bfdb7cad74669fb90e5679a2f17c30c18bf223b
Max <maximilian.paulsen@gmail.com>
2019-03-31 19:58:55 -0700

    Added ability to advance to the next frame header and fixed paths

--- src/Frame.c
+++ src/Frame.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#define FRAME_HEADER_BYTE_1 0xFF
+#define FRAME_HEADER_BYTE_2 0xFB
 
-int findFrameHeader(FILE *pFile, char *output, int maxRead){
-  return 0;
+/**
+ * Advances a file pointer to the next frame header writing the preceding
+ * data to output.
+ *
+ * @param pFile the file to read from
+ * @param output the output data to write to
+ * @param maxRead the maximum number of bytes to read
+ * @return the number of bytes read or EOF
+ */
+int advanceToFrameHeader(FILE *pFile, char *output, int maxRead){
+  int charGot;
+  int bytesRead = 0;
+  do {
+    charGot = fgetc(pFile);
+    output[bytesRead] = charGot;
+
+    if(charGot == FRAME_HEADER_BYTE_1 && (bytesRead + 1) < maxRead){
+      bytesRead++;
+      charGot = fgetc(pFile);
+      output[bytesRead] = charGot;
+      if(charGot == FRAME_HEADER_BYTE_2){
+        return bytesRead;
+      }
+    }
+    
+    bytesRead++;
+  } while (charGot != EOF && bytesRead < maxRead);
+  if(charGot == EOF){
+    return EOF;
+  }
+  return bytesRead;
 }
--- test/FrameTest.cpp
+++ test/FrameTest.cpp
@@ -4,8 +4,29 @@
 
 TEST(Frame, FindsFirstFrameInFile){
   FILE * pFile;
-  pFile = fopen("../testMusic/300HzSine.mp3", "r");
-  char tmpOutput[0xFF];
-  int start = findFrameHeader(pFile, &tmpOutput[0], 0xFF);
+  pFile = fopen(TEST_MUSIC"300HzSine.mp3", "r");
+  if(pFile == NULL){
+    fprintf(stderr, "Error opening File\n");
+    FAIL();
+    return;
+  }
+  char tmpOutput[0x1FF];
+  int start = advanceToFrameHeader(pFile, &tmpOutput[0], 0x1FF);
+  ASSERT_EQ(0x1, start);
+  fclose(pFile);
+}
+
+TEST(Frame, FindsSecondFrameInFile){
+  FILE * pFile;
+  pFile = fopen(TEST_MUSIC"300HzSine.mp3", "r");
+  if(pFile == NULL){
+    fprintf(stderr, "Error opening File\n");
+    FAIL();
+    return;
+  }
+  fgetc(pFile);
+  char tmpOutput[0x1FF];
+  int start = advanceToFrameHeader(pFile, &tmpOutput[0], 0x1FF);
   ASSERT_EQ(0x1A1, start);
+  fclose(pFile);
 }
